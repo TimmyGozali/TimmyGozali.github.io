19fdc233fb5dcc17a268a81a7b1f37e578c31ef8
TimmyGozali <106307127+TimmyGozali@users.noreply.github.com>
2022-05-26 02:58:06 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,7 +1,9 @@
-$.getJSON("https://raw.githubusercontent.com/TimmyGozali/FirstProject/main/test.json", function(json) {
-    console.log(json); 
-});
+function getjson() {
+    $.getJSON("https://raw.githubusercontent.com/TimmyGozali/FirstProject/main/test.json", function(json) {
+        console.log(json); 
+    });
+}
 
 function add(a, b) {
     return a+b
-    }
+}
