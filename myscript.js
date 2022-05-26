12cc2fc34283237496b1e3538f2c604989dc040b
TimmyGozali <106307127+TimmyGozali@users.noreply.github.com>
2022-05-26 03:01:56 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,7 +1,7 @@
 function getjson() {
-    $.getJSON("https://raw.githubusercontent.com/TimmyGozali/FirstProject/main/test.json", function(json) {
-        console.log(json); 
-    });
+    fetch('https://raw.githubusercontent.com/TimmyGozali/FirstProject/main/test.json')
+    .then(response => response.json())
+    .then(data => console.log(data));
 }
 
 function add(a, b) {
