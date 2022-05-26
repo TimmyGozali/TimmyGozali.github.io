32cb630945df0ec35bb656baca80ea4df71e8d7e
TimmyGozali <106307127+TimmyGozali@users.noreply.github.com>
2022-05-26 03:26:57 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,9 +1,13 @@
-function getjson() {
-    fetch('https://raw.githubusercontent.com/TimmyGozali/FirstProject/main/test.json')
+var skills;
+
+fetch('https://raw.githubusercontent.com/TimmyGozali/FirstProject/main/test.json')
     .then(response => response.json())
-    .then(data => console.log(data));
-}
+    .then(data => skills = data);
+    //.then(() =)> console.log(data));
+
+
 
 function add(a, b) {
     return a+b
-}
+    }
+
