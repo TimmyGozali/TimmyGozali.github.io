4ef26d6fe94d3e0a7c9720a0bb79f642f5a8a302
TimmyGozali <106307127+TimmyGozali@users.noreply.github.com>
2022-06-18 01:15:27 -0700

    Update calculate.js

--- javascripts/calculate.js
+++ javascripts/calculate.js
@@ -1,3 +1,20 @@
 function efficiency(a, b, reduction, sp) {
   return ((b / a) ** (reduction / sp))
 }
+
+/*
+var choice =  prompt("Level");
+var num = 'A' + choice;
+var num2 = 'Co' + (choice - 1);
+
+console.log(choice);
+console.log(num);
+console.log(num2);
+
+var dA = skills.TapDmg.A1;
+var dB = skills.TapDmg[num];
+var red = damage['Shadow Clone'].Tap;
+var cost = skills.TapDmg[num2];
+
+efficiency(dA, dB, red, cost);
+*/
