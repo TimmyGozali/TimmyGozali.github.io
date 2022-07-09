a2352558ba70851edabf85403a4a885d482f0e11
TimmyGozali <106307127+TimmyGozali@users.noreply.github.com>
2022-07-08 19:02:33 -0700

    Create ManaSiphon.js

---
+++ javascripts/ManaSiphon.js
@@ -0,0 +1,132 @@
+const ManaSiphon = {
+   "0": {
+      "SP cost": 0,
+      "Cumulative cost": 0,
+      "Siphon amount": 0
+   },
+   "1": {
+      "SP cost": 1,
+      "Cumulative cost": 1,
+      "Siphon amount": 1.4
+   },
+   "2": {
+      "SP cost": 1,
+      "Cumulative cost": 2,
+      "Siphon amount": 1.596
+   },
+   "3": {
+      "SP cost": 2,
+      "Cumulative cost": 4,
+      "Siphon amount": 1.827
+   },
+   "4": {
+      "SP cost": 2,
+      "Cumulative cost": 6,
+      "Siphon amount": 2.086
+   },
+   "5": {
+      "SP cost": 2,
+      "Cumulative cost": 8,
+      "Siphon amount": 2.378
+   },
+   "6": {
+      "SP cost": 2,
+      "Cumulative cost": 10,
+      "Siphon amount": 2.717
+   },
+   "7": {
+      "SP cost": 3,
+      "Cumulative cost": 13,
+      "Siphon amount": 3.109
+   },
+   "8": {
+      "SP cost": 3,
+      "Cumulative cost": 16,
+      "Siphon amount": 3.55
+   },
+   "9": {
+      "SP cost": 3,
+      "Cumulative cost": 19,
+      "Siphon amount": 4.056
+   },
+   "10": {
+      "SP cost": 4,
+      "Cumulative cost": 23,
+      "Siphon amount": 4.636
+   },
+   "11": {
+      "SP cost": 4,
+      "Cumulative cost": 27,
+      "Siphon amount": 5.298
+   },
+   "12": {
+      "SP cost": 5,
+      "Cumulative cost": 32,
+      "Siphon amount": 6.054
+   },
+   "13": {
+      "SP cost": 6,
+      "Cumulative cost": 38,
+      "Siphon amount": 6.926
+   },
+   "14": {
+      "SP cost": 7,
+      "Cumulative cost": 45,
+      "Siphon amount": 7.917
+   },
+   "15": {
+      "SP cost": 8,
+      "Cumulative cost": 53,
+      "Siphon amount": 9.052
+   },
+   "16": {
+      "SP cost": 9,
+      "Cumulative cost": 62,
+      "Siphon amount": 10.35
+   },
+   "17": {
+      "SP cost": 11,
+      "Cumulative cost": 73,
+      "Siphon amount": 11.842
+   },
+   "18": {
+      "SP cost": 12,
+      "Cumulative cost": 85,
+      "Siphon amount": 13.551
+   },
+   "19": {
+      "SP cost": 14,
+      "Cumulative cost": 99,
+      "Siphon amount": 15.505
+   },
+   "20": {
+      "SP cost": 16,
+      "Cumulative cost": 115,
+      "Siphon amount": 17.735
+   },
+   "21": {
+      "SP cost": 18,
+      "Cumulative cost": 133,
+      "Siphon amount": 20.296
+   },
+   "22": {
+      "SP cost": 21,
+      "Cumulative cost": 154,
+      "Siphon amount": 23.229
+   },
+   "23": {
+      "SP cost": 24,
+      "Cumulative cost": 178,
+      "Siphon amount": 26.58
+   },
+   "24": {
+      "SP cost": 28,
+      "Cumulative cost": 206,
+      "Siphon amount": 30.424
+   },
+   "25": {
+      "SP cost": 32,
+      "Cumulative cost": 238,
+      "Siphon amount": 34.82
+   }
+}
