57944b3057bd46217e89f938fc03e6dfd7eb1bc6
TimmyGozali <106307127+TimmyGozali@users.noreply.github.com>
2022-07-14 18:02:50 -0700

    Added sumCost groups

--- javascripts/manacalc.js
+++ javascripts/manacalc.js
@@ -84,6 +84,55 @@ function populate() {
     for (var j = 1; j < table.rows[i].cells.length; j++) {
       
       table.rows[i].cells[j].innerHTML = calcMana(i-3,j-1);
+
+      var sumCost = limit[i-3]["Cumulative cost"] + siphon[j-1]["Cumulative cost"];
+      
+      switch (sumCost) {
+        case (sumCost < 20):
+          table.rows[i].cells[j].classList.add('group-1');
+          break;
+        case (sumCost < 40):
+          table.rows[i].cells[j].classList.add('group-2');
+          break;
+        case (sumCost < 60):
+          table.rows[i].cells[j].classList.add('group-3');
+          break;
+        case (sumCost < 80):
+          table.rows[i].cells[j].classList.add('group-4');
+          break;
+        case (sumCost < 100):
+          table.rows[i].cells[j].classList.add('group-5');
+          break;
+        case (sumCost < 120):
+          table.rows[i].cells[j].classList.add('group-6');
+          break;
+        case (sumCost < 140):
+          table.rows[i].cells[j].classList.add('group-7');
+          break;
+        case (sumCost < 160):
+          table.rows[i].cells[j].classList.add('group-8');
+          break;
+        case (sumCost < 180):
+          table.rows[i].cells[j].classList.add('group-9');
+          break;
+        case (sumCost < 200):
+          table.rows[i].cells[j].classList.add('group-10');
+          break;
+        case (sumCost < 220):
+          table.rows[i].cells[j].classList.add('group-11');
+          break;
+        case (sumCost < 240):
+          table.rows[i].cells[j].classList.add('group-12');
+          break;
+        case (sumCost < 260):
+          table.rows[i].cells[j].classList.add('group-13');
+          break;
+        case (sumCost < 280):
+          table.rows[i].cells[j].classList.add('group-14');
+          break;
+        case (sumCost >= 280):
+          table.rows[i].cells[j].classList.add('group-15');
+      }
     }
   }
 }
